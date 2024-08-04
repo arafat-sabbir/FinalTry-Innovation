599c33d6f1e4af17146a0bad8d10decc6e1f6c6d
arafat-sabbir <arafatshabbir8@gmail.com>
2024-08-04 17:29:57 +0600

    fixed contact us section

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -6,17 +6,17 @@ import ContactForm from "./form/ContactForm";
 const ContactUs = () => {
 
   return (
-    <section className="bg-[url('/assets/background/contact_bg.png')] bg-no-repeat bg-cover dark:bg-opacity-50">
+    <section className="bg-[url('/assets/background/contact_bg.png')] bg-no-repeat bg-cover dark:bg-opacity-50  pb-20 ">
       <Container>
         <SectionHeading
           foreGroundText="CONTACT US"
           backgroundText="GET IN TOUCH"
         />
-        <div className="lg:w-7/12  pb-20 lg:mx-auto xl:mx-0">
-          <h1 className="font-semibold text-4xl text-center xl:text-start  text-black dark:text-white">
+        <div className="lg:w-7/12 lg:mx-auto xl:mx-0 dark:shadow-[0_0_10px_#070a13] dark:bg-[#030A1C] dark:p-10 p-10 lg:p-0 rounded-md">
+          <h1 className="font-semibold text-4xl text-center xl:text-start  text-black dark:text-gray-300  ">
             Have Any Questions?
           </h1>
-          <p className="my-6 text-center lg:text-start">
+          <p className="my-6 text-center lg:text-start text-black dark:text-gray-300">
             Enthusiastically disintermediate one-to-one leadership via business
             e-commerce. Dramatically reintermediate compelling process
             improvements rather than empowered relationships.
