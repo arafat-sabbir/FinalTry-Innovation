07fe94b8aafa78c40bfd2c30885c4be88587ab29
arafat-sabbir <arafatshabbir8@gmail.com>
2025-06-26 10:17:01 +0600

    update footer year for copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -140,7 +140,7 @@ const Footer = () => {
           <hr className="h-px my-6 bg-gray-200 border-none dark:bg-gray-700" />
           <div>
             <p className="text-center text-gray-500 dark:text-gray-400">
-              © Brand 2024 - All rights reserved
+              &copy; {new Date().getFullYear()} - All rights reserved
             </p>
           </div>
         </div>
